dfebec2a3165b85202a0e5ece8b0567dc452b83d
Saurav <saurav77k@gmail.com>
2025-10-19 15:37:33 +0530

    new comment added in classes.java file.

--- JavaConceptLearning/src/learning/Classes.java
+++ JavaConceptLearning/src/learning/Classes.java
@@ -2,6 +2,7 @@
  * Object is a instance of a class. Object and instance can be used interchangeably.
  * The data or variable defined in a class are called instance variable.
  * dot(.) operator is used to access the variables from the object.
+ * There can be only one public class in a single .java file and the class name must match the filename (without the .java extension).
  * */
 package learning;
 
